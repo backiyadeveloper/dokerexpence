29adeedec858afd050bdb9ad8b7e390887219b13
backiyadeveloper <backiya3011@gmail.com>
2025-07-23 15:37:55 +0530

    docker image creation and modify docker compose file

--- expense-tracker/src/main/java/com/example/expense/expense_tracker/jwtconfig/SecurityConfig.java
+++ expense-tracker/src/main/java/com/example/expense/expense_tracker/jwtconfig/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig implements WebMvcConfigurer {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+		configuration.setAllowedOrigins(List.of("http://localhost:3000","http://localhost:5173"));
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 		configuration.setExposedHeaders(List.of("Authorization"));
